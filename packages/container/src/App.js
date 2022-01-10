f788c60d9d6845edb4e5391a513145322be6dd22
Daniel Simao <simaodeveloper@gmail.com>
2022-01-10 06:36:13 -0300

    feat: fix container text

--- packages/container/src/App.js
+++ packages/container/src/App.js
@@ -4,7 +4,7 @@ import MarketingApp from "../components/MarketingApp";
 const App = () => {
   return (
     <div>
-      <h1>Hi Theres!</h1>
+      <h1>Hi Theress!</h1>
       <hr />
       <MarketingApp />
     </div>
